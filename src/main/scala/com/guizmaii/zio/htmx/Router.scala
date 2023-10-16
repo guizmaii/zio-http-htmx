e3f0cc90551b64187548b80045a48fb93e39a974
jules Ivanic <jules.ivanic@gmail.com>
2023-10-16 15:55:57 +0400

    Clean code

--- src/main/scala/com/guizmaii/zio/htmx/Router.scala
+++ src/main/scala/com/guizmaii/zio/htmx/Router.scala
@@ -11,22 +11,16 @@ object Router {
     Http.collectZIO[Request] {
       case req @ Method.GET -> Root     =>
         ZIO.serviceWithZIO[SessionManager] { sessionManager =>
-          req.url.queryParams.get("search").flatMap(_.headOption).map(_.trim) match {
-            case None         =>
-              for {
-                maybeLoggedUser <- sessionManager.loggedUser(req)
-              } yield Response.twirl(views.html.index(maybeLoggedUser)())
-            case Some(filter) =>
-              for {
-                users           <- ZIO.serviceWith[UsersService](_.find(filter))
-                maybeLoggedUser <- sessionManager.loggedUser(req)
-              } yield
-                if (req.hasHeader("HX-Request")) Response.twirl(partials.html.users_table_body(users))
-                else
-                  Response.twirl(
-                    views.html.index(maybeLoggedUser)(searchValue = Some(filter), users = Some(users))
-                  )
-          }
+          for {
+            filter          <- ZIO.succeed(req.url.queryParams.get("search").flatMap(_.headOption).fold(ifEmpty = "")(_.trim))
+            users           <- ZIO.serviceWith[UsersService](_.find(filter))
+            maybeLoggedUser <- ZIO.serviceWithZIO[SessionManager](_.loggedUser(req))
+          } yield
+            if (req.hasHeader("HX-Request")) Response.twirl(partials.html.users_table_body(users))
+            else
+              Response.twirl(
+                views.html.index(maybeLoggedUser)(searchValue = Some(filter), users = Some(users))
+              )
         }
       case Method.GET -> Root / "blog"  => ZIO.succeed(Response.twirl(views.html.blog()))
       case Method.GET -> Root / "names" => ZIO.succeed(Response.twirl(partials.html.names(List("Pierre", "Paul", "Jacques"))))
